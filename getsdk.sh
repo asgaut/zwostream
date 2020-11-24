42f62112e7da159c59f138a051451e77bfb94861
Asgaut Eng <asgaut@gmail.com>
2020-11-24 22:08:58 +0000

    Make wget more --quiet for use in non-terminals

--- getsdk.sh
+++ getsdk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 NAME=ASI_linux_mac_SDK_V1.15.0915.tar.bz2
-wget https://astronomy-imaging-camera.com/software/$NAME
+wget --quiet https://astronomy-imaging-camera.com/software/$NAME
 mkdir sdk
 tar xvf $NAME -C sdk --strip 1
 rm $NAME
